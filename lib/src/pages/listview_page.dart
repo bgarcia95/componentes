ba00f8940c7bcf00273aadbb985acb54543be7cb
bgarcia95 <bgarcia.95@outlook.com>
2019-10-14 00:13:23 -0600

    Adding infinite scroll with futures

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+
 class ListaPage extends StatefulWidget {
   static const String id = 'list';
   @override
@@ -12,18 +14,28 @@ class _ListaPageState extends State<ListaPage> {
 
   List<int> _listaNumeros = [];
   int _ultimoNumero = 0;
+  bool _isLoading = false;
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     _agregar10();
 
     // Infinite Scroll in action
-    _scrollController.addListener((){
-      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-        _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        // _agregar10();
+        // Infinite Scroll con Futures
+        fetchData();
       }
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 
   @override
@@ -32,7 +44,12 @@ class _ListaPageState extends State<ListaPage> {
       appBar: AppBar(
         title: Text('Listas'),
       ),
-      body: _crearLista(),
+      body: Stack(
+        children: <Widget>[
+          _crearLista(),
+          _crearLoading(),
+        ],
+      ),
     );
   }
 
@@ -57,9 +74,47 @@ class _ListaPageState extends State<ListaPage> {
       _listaNumeros.add(_ultimoNumero);
     }
 
-    setState(() {
-      
-    });
+    setState(() {});
   }
 
+  // Infinite Scroll con Futures
+  Future<Null> fetchData() async {
+    _isLoading = true;
+    setState(() {});
+
+    final duration = new Duration(seconds: 2);
+    return new Timer(duration, respuestaHTTP);
+  }
+  
+  // Infinite Scroll con Futures
+  void respuestaHTTP() {
+    _isLoading = false;
+
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 100,
+      curve: Curves.fastOutSlowIn,
+      duration: Duration(milliseconds: 250)
+    );
+    _agregar10();
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+            ],
+          ),
+          SizedBox(height: 15.0),
+        ],
+      );
+    } else {
+      return Container();
+    }
+  }
 }
