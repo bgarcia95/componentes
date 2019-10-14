f946064210a9544c492bb849b61e7ded776e24a1
bgarcia95 <bgarcia.95@outlook.com>
2019-10-13 19:11:27 -0600

    finished animatedContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
 
 void main() => runApp(MyApp());
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,9 +1,10 @@
-import 'package:componentes/src/pages/card_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/card_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -11,5 +12,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     AlertPage.id: (BuildContext context) => AlertPage(),
     AvatarPage.id: (BuildContext context) => AvatarPage(),
     CardPage.id: (BuildContext context) => CardPage(),
+    AnimatedContainerPage.id: (BuildContext context) => AnimatedContainerPage(),
   };
 }
